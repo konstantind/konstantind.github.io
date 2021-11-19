9bf4bcee507cb7dc41d074de549896ad096c65e2
Konstantin D <merty.doc@gmail.com>
2021-11-19 13:18:31 +0300

    Deploy website - based on bbe3cefda6e72eed927966cc81ba542612fb0d30

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/konstantindubelev/Projects/konstantind.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/konstantindubelev/Projects/konstantind.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/konstantindubelev/Projects/konstantind.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/konstantindubelev/Projects/konstantind.github.io/src/css/custom.css"),
-];
